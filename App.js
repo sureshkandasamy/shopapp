8fd0fd8b4fb1dae9227bb8d713016c36bdaf5d4f
Suresh <ksuresh2k@gmail.com>
2017-12-30 10:43:19 +1100

    Added button and some network sample calls

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {username: '', pwd: '', btnPressed: 0};
+    this.submitClick = this.submitClick.bind(this);
+    this.getMoviesFromApiAsync = this.getMoviesFromApiAsync.bind(this);
+  }
+
+  submitClick()
+  {
+    alert('submit clicked');
+    this.setState({username : 'Test User'});
+    // this.setState({username:this.state.username})
+    this.getMoviesFromApiAsync();
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.movies);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Text style={{ fontWeight: 'bold', fontSize:30, paddingBottom:60 }}>Login</Text>
+        </View>
+        <View style={{paddingBottom:40}}>
+          <Text style={{ fontWeight: 'bold', fontSize:20}}>Username:</Text>
+          <TextInput
+            style={{height: 40, width:100}}
+            placeholder="Username"
+            onChangeText={(username) => this.setState({username})}
+          />
+        </View>
+        <View>
+          <Text style={{ fontWeight: 'bold', fontSize:20}}>Password:</Text>
+          <TextInput
+            style={{height: 40, width:100}}
+            placeholder="Password"
+            onChangeText={(pwd) => this.setState({pwd})}
+          />
+        </View>
+        <Text>{this.state.username}</Text>
+        <View>
+          <Button
+            onPress={this.submitClick }
+            title="Login"
+            color="black"
+            accessibilityLabel="Press to Login"
+          />
+        </View>
       </View>
     );
   }
@@ -15,9 +66,11 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 60,
     alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'grey',
     justifyContent: 'center',
   },
 });
